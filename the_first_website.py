a23f51734529258edc6adafaada70095c84e676c
baraa2013 <baraa.ben.naim@gmail.com>
2025-03-18 17:45:32 +0100

    Update the_first_website.py

--- the_first_website.py
+++ the_first_website.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
+st.title('سلام')
+
+text_empty = st.empty()
+
 if st.button('إضغط'):
     text_empty.write('لقد ضغطت على الزر')
     
     if st.button('حذف'):
         text_empty.empty()
-
-text_empty = st.empty()
-st.title('سلام')
