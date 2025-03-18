acec6d09c76596b4bb841fa6924083f29e5219eb
baraa2013 <baraa.ben.naim@gmail.com>
2025-03-18 17:54:47 +0100

    Update the_first_website.py

--- the_first_website.py
+++ the_first_website.py
@@ -6,13 +6,9 @@ text_empty = st.empty()
 
 button_empty = st.empty()
 
-button2_empty = st.empty()
-
 if button_empty.button('إضغط'):
     text_empty.write('لقد ضغطت على الزر')
-    button2_empty.button('حذف')
-
-    if button2_empty:
-        button2_empty.empty()
-        button_empty.button('إضغط')
+    
+    if st.button('حذف'):
+        button_empty.empty()
         text_empty.empty()
