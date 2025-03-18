585d128700107949fc177af08c86910c85e09c4f
baraa2013 <baraa.ben.naim@gmail.com>
2025-03-18 17:50:29 +0100

    Update the_first_website.py

--- the_first_website.py
+++ the_first_website.py
@@ -4,8 +4,15 @@ st.title('سلام')
 
 text_empty = st.empty()
 
-if st.button('إضغط'):
+button_empty = st.empty()
+
+button2_empty = st.empty()
+
+if button_empty.button('إضغط'):
     text_empty.write('لقد ضغطت على الزر')
-    
-    if st.button('حذف'):
+    button2_empty.button('حذف')
+
+    if button2_empty:
+        button2_empty.empty()
+        button_empty.button('إضغط')
         text_empty.empty()
